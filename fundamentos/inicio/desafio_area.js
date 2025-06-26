f63aad06d12b3ec58956a166e696e857ae6fbe11
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2025-06-26 19:53:47 -0300

    Initial commit

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// PI = 3.141592
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.14;
+let area = PI * raio * raio;
+
+console.log('O valor da área é ' + area + 'm2');
+
+area = Math.PI * raio * raio;
+console.log('O valor da área é ' + area + 'm2')
---
+++ fundamentos/inicio/sentenca_1.js
@@ -0,0 +1,6 @@
+console
+.log('Bom Dia!!')
+
+console.log('Boa Tarde!!')
+
+console.log('Boa Noite!!')
