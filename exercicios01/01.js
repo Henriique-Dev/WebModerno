ef05c581f6302b2cebff47b2fb4eb919db985b36
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2025-07-01 20:50:27 -0300

    01/07

---
+++ exercicios01/01.js
@@ -0,0 +1,16 @@
+const calc = function(x, y) {
+    let soma = x + y
+    console.log(`Soma: ${soma}`)
+
+    let sub = x - y
+    console.log(`Subtração: ${sub}`)
+
+    let mult = x * y
+    console.log(`Multiplicação: ${mult}`)
+
+    let div = x / 2
+    console.log(`Divisão: ${div}`)
+}
+
+calc(3, 4)
+calc(5, 5)
