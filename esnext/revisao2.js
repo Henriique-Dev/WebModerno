34c475c4eb90bc11b087f4bf51180a8008446902
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2025-09-14 13:51:37 -0300

    09/25

---
+++ esnext/revisao2.js
@@ -0,0 +1,19 @@
+// arrow function
+const soma = (a, b) => a + b
+console.log( soma( 2, 3))
+
+// arrow function (this)
+const lexico1 = () => console.log(this === exports)
+const lexico2 = lexico1.bind({})
+lexico1()
+lexico2()
+
+// parametro default
+function log( texto = 'Node') {
+    console.log(texto)
+}
+
+log()
+log('Sou mais forte')
+
+// operador rest
