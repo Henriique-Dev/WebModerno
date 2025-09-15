aff414eff1c0ccb8695a69eca1a23f0a0e9f490d
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2025-09-14 21:38:09 -0300

    09/25

--- esnext/revisao2.js
+++ esnext/revisao2.js
@@ -17,3 +17,9 @@ log()
 log('Sou mais forte')
 
 // operador rest
+function total(...numeros) {
+    let total = 0
+    numeros.forEach( n => total += n)
+    return total
+}
+console.log(total(2, 3, 4, 5))
