a60ec4dc6badf498692089d7aac04ec4d876c29b
Henriique-Dev <vitorhenriquexaviier@gmail.com>
2025-10-28 12:36:49 -0300

    28/10

--- esnext/forOf.js
+++ esnext/forOf.js
@@ -29,3 +29,12 @@ for (let chave of assuntosMap.keys()) {
 for (let valor of assuntosMap.values()) {
     console.log(valor)
 }
+
+for (let [ch, vl] of assuntosMap.entries()) {
+    console.log(ch, vl)
+}
+
+const s = new Set(['a', 'b', 'c'])
+for (let letra of s) {
+    console.log(letra)
+}
